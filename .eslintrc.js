282adee2a5ebe5684272fda21d751028a43a2d50
Nofrisdan <nofrisdansitopu22@gmail.com>
2022-09-23 08:58:03 +0700

    Konfigurasi depedensi react-native

---
+++ .eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  root: true,
+  'prettier/prettier': ['error', {endOfLine: 'auto'}],
+  extends: '@react-native-community',
+};
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import styles from './Assets/style';
+const App = () => {
+  return (
+    <View style={styles.centerDisplay}>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
